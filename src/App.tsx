c7d66eaaef77b8bf0d1bd05a3eea3d0e49c6a74f
DaroMacs <dariomacedog@gmail.com>
2024-08-21 16:21:46 -0600

    Styles arranged

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,18 @@ function App() {
 
   return (
     <>
+      <h1 className="line">Yolanda Suárez</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+        perferendis aliquid maiores voluptas officia officiis, odit vero fugit
+        repellat esse accusamus. Quasi non maxime ducimus qui fuga architecto,
+        ut tempore? Repudiandae nisi, earum impedit provident tempora veritatis
+        odit. Fugit et molestias quibusdam dolore fugiat, minima animi numquam
+        quia culpa laboriosam nihil porro, deserunt quae labore error sequi
+        dicta, voluptatum reiciendis!
+      </p>
+
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,11 +28,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hola Yoda</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        {/* <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </button> */}
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
