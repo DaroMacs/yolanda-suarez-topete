e92fe3dd282129ed207bec0e95b306318fc1b2e9
DaroMacs <dariomacedog@gmail.com>
2024-10-08 18:17:28 -0600

    Fixes carousel

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
@@ -13,13 +14,67 @@ import poster7 from "./assets/7.webp";
 import poster8 from "./assets/8.webp";
 import poster9 from "./assets/9.webp";
 
+const imageText = [
+  {
+    id: 1,
+    text: "Lorem 1 ipsum dolor sit amet consectetur adipisicing elit. Maxime temporibus nam assumenda, perferendis eum eos sequi aliquid vero ratione. Nulla asperiores autem alias tenetur eaque saepe qui quae, consequatur consectetur!",
+  },
+  {
+    id: 2,
+    text: "Lorem 2 ipsum dolor sit amet consectetur adipisicing elit. Maxime temporibus nam assumenda, perferendis eum eos sequi aliquid vero ratione. Nulla asperiores autem alias tenetur eaque saepe qui quae, consequatur consectetur!",
+  },
+  {
+    id: 3,
+    text: "Lorem 3 ipsum dolor sit amet consectetur adipisicing elit. Maxime temporibus nam assumenda, perferendis eum eos sequi aliquid vero ratione. Nulla asperiores autem alias tenetur eaque saepe qui quae, consequatur consectetur!",
+  },
+  {
+    id: 4,
+    text: "Lorem 4 ipsum dolor sit amet consectetur adipisicing elit. Maxime temporibus nam assumenda, perferendis eum eos sequi aliquid vero ratione. Nulla asperiores autem alias tenetur eaque saepe qui quae, consequatur consectetur!",
+  },
+  {
+    id: 5,
+    text: "Lorem 5 ipsum dolor sit amet consectetur adipisicing elit. Maxime temporibus nam assumenda, perferendis eum eos sequi aliquid vero ratione. Nulla asperiores autem alias tenetur eaque saepe qui quae, consequatur consectetur!",
+  },
+  {
+    id: 6,
+    text: "Lorem 6 ipsum dolor sit amet consectetur adipisicing elit. Maxime temporibus nam assumenda, perferendis eum eos sequi aliquid vero ratione. Nulla asperiores autem alias tenetur eaque saepe qui quae, consequatur consectetur!",
+  },
+  {
+    id: 7,
+    text: "Lorem 7 ipsum dolor sit amet consectetur adipisicing elit. Maxime temporibus nam assumenda, perferendis eum eos sequi aliquid vero ratione. Nulla asperiores autem alias tenetur eaque saepe qui quae, consequatur consectetur!",
+  },
+  {
+    id: 8,
+    text: "Lorem 8 ipsum dolor sit amet consectetur adipisicing elit. Maxime temporibus nam assumenda, perferendis eum eos sequi aliquid vero ratione. Nulla asperiores autem alias tenetur eaque saepe qui quae, consequatur consectetur!",
+  },
+  {
+    id: 9,
+    text: "Lorem 9 ipsum dolor sit amet consectetur adipisicing elit. Maxime temporibus nam assumenda, perferendis eum eos sequi aliquid vero ratione. Nulla asperiores autem alias tenetur eaque saepe qui quae, consequatur consectetur!",
+  },
+  {
+    id: 10,
+    text: "Lorem 10 ipsum dolor sit amet consectetur adipisicing elit. Maxime temporibus nam assumenda, perferendis eum eos sequi aliquid vero ratione. Nulla asperiores autem alias tenetur eaque saepe qui quae, consequatur consectetur!",
+  },
+];
+
 export default function SimpleSlider() {
+  const [slide, setSlide] = useState(1);
+
+  const afterChange = (i: number) => {
+    setSlide(i + 1);
+  };
+
+  console.log(slide);
+
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    afterChange: afterChange,
+    centerMode: true,
+    centerPadding: "70px",
   };
   return (
     <div className="image-slider-container">
@@ -55,6 +110,10 @@ export default function SimpleSlider() {
           <img src={poster10} alt="poster10" />
         </div>
       </Slider>
+      <div className="slide-counter">
+        <p>{slide}/10</p>
+        <p className="text">{imageText[slide - 1].text}</p>
+      </div>
     </div>
   );
 }
